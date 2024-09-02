495be8f996969d3d85b8ad0c1bd475c544958a38
Ziyi Chu <87994486+RobertCHU01@users.noreply.github.com>
2024-09-01 20:59:36 -0500

    double cycle to achieve board

--- src/App.js
+++ src/App.js
@@ -37,23 +37,23 @@ function Board({ xIsNext, squares, onPlay, move }) {
     <>
       <div className="status"> {status} </div>
       <div className="status2"> {status2} </div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {Array(3).fill(null).map((_, rowIndex) => (
+        <div key={rowIndex} className="board-row">
+          {Array(3).fill(null).map((_, colIndex) => {
+            const index = rowIndex * 3 + colIndex;
+            return (
+              <Square
+                key={index}
+                value={squares[index]}
+                onSquareClick={() => handleClick(index)}
+              />
+            );
+          })}
+        </div>
+      ))}
     </>
   );
+  
 }
 
 function calculateWinner(squares) {
